57be47c5d170d18c1664ae7150066ad9469592c3
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-17 18:39:09 -0500

    Added background color function for mouseover

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 // Add a 16x16 grid of square divs
 
+const divSquare = document.createElement("div");
+
 for (i = 1; i < 257; i++) {
 
-    const divSquare = document.createElement("div");
+    let divSquare = document.createElement("div");
     divSquare.classList.add('div-square');
-    const div1Text = document.createTextNode(" ");
+    let div1Text = document.createTextNode(" ");
     divSquare.appendChild(div1Text);
     const element = document.getElementById("container");
     element.appendChild(divSquare);
 
 }
+
+// const testDiv = document.getElementById("test");
+
+// function changeBackgroundColor() {
+//     testDiv.setAttribute('style', 'background-color: black;');
+//  }
+
+
+// testDiv.addEventListener("mouseover", () => changeBackgroundColor());
+
+const squareElements = document.querySelectorAll('div.div-square');
+
+
+function changeBackgroundColor() {
+    squareElements.setAttribute('style', 'background-color: black;');
+ }
+
+
+squareElements.addEventListener("mouseover", () => changeBackgroundColor());
+
+
+
+
+
+
