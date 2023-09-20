c8f6f796e7294082a6d447013191fbe208ecc456
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-19 20:46:04 -0500

    Added drawing feature and new grid button

--- script.js
+++ script.js
@@ -1,39 +1,51 @@
 // Add a 16x16 grid of square divs
 
-const divSquare = document.createElement("div");
+const element = document.getElementById("container");
 
 for (i = 1; i < 257; i++) {
+    let div1 = document.createElement("div");
+    div1.classList.add("div-square");
+    element.appendChild(div1);
+}
 
-    let divSquare = document.createElement("div");
-    divSquare.classList.add('div-square');
-    let div1Text = document.createTextNode(" ");
-    divSquare.appendChild(div1Text);
-    const element = document.getElementById("container");
-    element.appendChild(divSquare);
+let divSquare = document.getElementsByClassName("div-square");
+
+// When user mouseovers cells, make background of each cell black
+    for (let i = 0; i < divSquare.length; i++) {
+        divSquare[i].addEventListener("mouseover", function changeBackgroundColor() {
+            divSquare[i].style.backgroundColor = "black";
+        });
+      }
+
+// Ask user for number of squares per side for the new grid. 
+
+let gridSize;
+let gridSizeInteger;
+
+function askForGridSize() {
+     gridSize = prompt("Enter number of squares per side for sketch pad:");
+
+    // Set the limit for the user input to a maximum of 100.
+    gridSizeInteger = parseInt(gridSize);
+    if (gridSizeInteger < 0 || gridSizeInteger > 100) gridSizeInteger = "ERROR";
+
+    // Remove existing grid
+    while(divSquare[0]) {
+        divSquare[0].parentNode.removeChild(divSquare[0]);
+    }
+
+    let gridSideSize = gridSizeInteger*gridSizeInteger + 1;
+
+    // Create new grid in same total space as before for new sketch pad
+    for (i = 1; i < gridSideSize; i++) {
+        let div1 = document.createElement("div");
+        div1.classList.add("div-square");
+        element.appendChild(div1);
+    }
 
 }
 
-// const testDiv = document.getElementById("test");
-
-// function changeBackgroundColor() {
-//     testDiv.setAttribute('style', 'background-color: black;');
-//  }
-
-
-// testDiv.addEventListener("mouseover", () => changeBackgroundColor());
-
-const squareElements = document.querySelectorAll('div.div-square');
-
-
-function changeBackgroundColor() {
-    squareElements.setAttribute('style', 'background-color: black;');
- }
-
-
-squareElements.addEventListener("mouseover", () => changeBackgroundColor());
-
-
-
-
+// Assign button to javascript variable
+const gridSizeButton = document.getElementById("button").onclick = askForGridSize;
 
 
